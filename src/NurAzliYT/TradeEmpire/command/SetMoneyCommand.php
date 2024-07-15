420b6ca5a6b8f884185b536872f2b60e3791f93c
NurAzli <144025939+NurAzli@users.noreply.github.com>
2024-07-15 18:02:41 +0700

    Update SetMoneyCommand.php

--- src/NurAzliYT/TradeEmpire/command/SetMoneyCommand.php
+++ src/NurAzliYT/TradeEmpire/command/SetMoneyCommand.php
@@ -7,21 +7,23 @@ use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\plugin\PluginOwnedTrait;
-use pocketmine\Server;
-use NurAzliYT\TradeEmpire\Main;
+use NurAzliYT\TradeEmpire\TradeEmpire;
 
-class SetMoneyCommand extends Command implements PluginOwned {
+class SetMoneyCommand extends Command implements PluginOwned
+{
     use PluginOwnedTrait;
 
-    private $plugin;
+    private TradeEmpire $plugin;
 
-    public function __construct(Main $plugin) {
+    public function __construct(TradeEmpire $plugin)
+    {
         parent::__construct("setmoney", "Set a player's balance", "/setmoney <player> <amount>", []);
         $this->setPermission("tradeempire.command.setmoney");
         $this->plugin = $plugin;
     }
 
-    public function execute(CommandSender $sender, string $label, array $args): bool {
+    public function execute(CommandSender $sender, string $label, array $args): bool
+    {
         if (!$this->testPermission($sender)) {
             return false;
         }
@@ -31,15 +33,23 @@ class SetMoneyCommand extends Command implements PluginOwned {
             return false;
         }
 
-        $playerName = $args[0];
-        $amount = intval($args[1]);
+        $playerName = array_shift($args);
+        $amount = array_shift($args);
 
-        $this->plugin->getEconomyManager()->setBalance($playerName, $amount);
-        $sender->sendMessage("Set $playerName's balance to $" . $amount);
-        return true;
+        $player = $this->plugin->getServer()->getPlayerByPrefix($playerName);
+
+        if ($player instanceof Player) {
+            $this->plugin->getEconomyManager()->setMoney($player, (float)$amount);
+            $sender->sendMessage("Set " . $player->getName() . "'s balance to $amount.");
+            return true;
+        } else {
+            $sender->sendMessage("Player not found.");
+            return false;
+        }
     }
 
-    public function getOwningPlugin(): Main {
+    public function getOwningPlugin(): TradeEmpire
+    {
         return $this->plugin;
     }
 }
