17ef85e06b579741679f75786b045d84825c9d8b
NurAzli <144025939+NurAzli@users.noreply.github.com>
2024-07-15 18:03:08 +0700

    Update RemoveMoneyCommand.php

--- src/NurAzliYT/TradeEmpire/command/RemoveMoneyCommand.php
+++ src/NurAzliYT/TradeEmpire/command/RemoveMoneyCommand.php
@@ -7,21 +7,23 @@ use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\plugin\PluginOwnedTrait;
-use pocketmine\Server;
-use NurAzliYT\TradeEmpire\Main;
+use NurAzliYT\TradeEmpire\TradeEmpire;
 
-class RemoveMoneyCommand extends Command implements PluginOwned {
+class RemoveMoneyCommand extends Command implements PluginOwned
+{
     use PluginOwnedTrait;
 
-    private $plugin;
+    private TradeEmpire $plugin;
 
-    public function __construct(Main $plugin) {
+    public function __construct(TradeEmpire $plugin)
+    {
         parent::__construct("removemoney", "Remove money from a player's balance", "/removemoney <player> <amount>", []);
         $this->setPermission("tradeempire.command.removemoney");
         $this->plugin = $plugin;
     }
 
-    public function execute(CommandSender $sender, string $label, array $args): bool {
+    public function execute(CommandSender $sender, string $label, array $args): bool
+    {
         if (!$this->testPermission($sender)) {
             return false;
         }
@@ -31,15 +33,23 @@ class RemoveMoneyCommand extends Command implements PluginOwned {
             return false;
         }
 
-        $playerName = $args[0];
-        $amount = intval($args[1]);
+        $playerName = array_shift($args);
+        $amount = array_shift($args);
 
-        $this->plugin->getEconomyManager()->removeBalance($playerName, $amount);
-        $sender->sendMessage("Removed $" . $amount . " from $playerName's balance");
-        return true;
+        $player = $this->plugin->getServer()->getPlayerByPrefix($playerName);
+
+        if ($player instanceof Player) {
+            $this->plugin->getEconomyManager()->removeMoney($player, (float)$amount);
+            $sender->sendMessage("Removed $amount from " . $player->getName() . "'s balance.");
+            return true;
+        } else {
+            $sender->sendMessage("Player not found.");
+            return false;
+        }
     }
-    
-    public function getOwningPlugin(): Main {
+
+    public function getOwningPlugin(): TradeEmpire
+    {
         return $this->plugin;
     }
 }
