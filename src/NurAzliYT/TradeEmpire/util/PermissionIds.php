2d0d6fc6ce89cb688dfdff2789fd6a92fb54db8a
NurAzli <144025939+NurAzli@users.noreply.github.com>
2024-07-15 18:37:19 +0700

    Create PermissionIds.php

---
+++ src/NurAzliYT/TradeEmpire/util/PermissionIds.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+namespace NurAzliYT\TradeEmpire\util;
+
+final class PermissionIds {
+  const ADDMONEY_COMMAND = "tradeempire.command.addmoney";
+  const BALANCE_COMMAND = "tradeempire.command.balance";
+  const REMOVEMONEY_COMMAND = "tradeempire.command.removemoney";
+  const SETMONEY_COMMAND = "tradeempire.command.setmoney";
+}
