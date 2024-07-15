cd21fc300895e30f0a97ba36463a10456f48afba
NurAzli <144025939+NurAzli@users.noreply.github.com>
2024-07-15 17:45:44 +0700

    Update Main.php

--- src/NurAzliYT/TradeEmpire/Main.php
+++ src/NurAzliYT/TradeEmpire/Main.php
@@ -1,45 +1,73 @@
 <?php
 
+/**
+ * MIT License
+ *
+ * Copyright (c) 2021-2024 NurAzliYT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @auto-license
+ */
+
+declare(strict_types=1);
+
 namespace NurAzliYT\TradeEmpire;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\player\Player;
 use pocketmine\utils\Config;
-use NurAzliYT\TradeEmpire\command\BalanceCommand;
-use NurAzliYT\TradeEmpire\command\SetMoneyCommand;
+use pocketmine\player\Player;
 use NurAzliYT\TradeEmpire\command\AddMoneyCommand;
+use NurAzliYT\TradeEmpire\command\BalanceCommand;
 use NurAzliYT\TradeEmpire\command\RemoveMoneyCommand;
+use NurAzliYT\TradeEmpire\command\SetMoneyCommand;
 use NurAzliYT\TradeEmpire\util\EconomyManager;
 
-class Main extends PluginBase {
+final class TradeEmpire extends PluginBase
+{
+    private EconomyManager $economyManager;
 
-    /** @var EconomyManager */
-    private $economyManager;
+    protected function onLoad(): void
+    {
+        $this->saveDefaultConfig();
+    }
 
-    public function onEnable(): void {
-        // Create balances file
+    protected function onEnable(): void
+    {
         @mkdir($this->getDataFolder());
         $this->economyManager = new EconomyManager($this);
+
         $this->registerCommands();
     }
 
-    private function registerCommands(): void {
-        $setMoneyCommand = new SetMoneyCommand($this);
-        $this->getServer()->getCommandMap()->register("tradeempire", $setMoneyCommand);
-        
-        $balanceCommand = new BalanceCommand($this);
-        $this->getServer()->getCommandMap()->register("tradeempire", $balanceCommand);
-
-        $addMoneyCommand = new AddMoneyCommand($this);
-        $this->getServer()->getCommandMap()->register("tradeempire", $addMoneyCommand);
-
-        $removeMoneyCommand = new RemoveMoneyCommand($this);
-        $this->getServer()->getCommandMap()->register("tradeempire", $removeMoneyCommand);
+    private function registerCommands(): void
+    {
+        $this->getServer()->getCommandMap()->registerAll("tradeempire", [
+            new BalanceCommand($this),
+            new SetMoneyCommand($this),
+            new AddMoneyCommand($this),
+            new RemoveMoneyCommand($this)
+        ]);
     }
 
-    public function getEconomyManager(): EconomyManager {
+    public function getEconomyManager(): EconomyManager
+    {
         return $this->economyManager;
     }
 }
